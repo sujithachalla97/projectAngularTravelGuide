5fd09b768661734393dc665b0fbb860057a93611
sujithachalla <sujitha.academic.97@gmail.com>
2024-10-27 19:00:55 +0530

    resolved conflicts between master and branch-sujithachalla

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent,SignupComponent],
+  
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  
 }
+
+
+
